77ffaf291b731e10b2b657deaa1931d9204063c5
javohir7777 <91466315+javohir7777@users.noreply.github.com>
2023-03-10 11:50:01 +0500

    +998(90)-
    2

--- src/pages/app-add-form/App-add-form.js
+++ src/pages/app-add-form/App-add-form.js
@@ -1,25 +1,45 @@
+import { Component } from "react";
 import "./App-add-form.css";
-function AppAddForm() {
-  return (
-    <div className="movie-add-form">
-      <h3>Yangi kino qo'shish</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Qanday kino?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="Necha marta ko'rilgan?"
-        />
-        <button type="submit" className="btn btn-outline-dark">
-          Qo'shish
-        </button>
-      </form>
-    </div>
-  );
+
+class AppAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", views: "" };
+  }
+  changeHandlerInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  render() {
+    const { name, views } = this.state;
+    return (
+      <div className="movie-add-form">
+        <h3>Yangi kino qo'shish</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Qanday kino?"
+            name="name"
+            onChange={this.changeHandlerInput}
+            value={name}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="Necha marta ko'rilgan?"
+            name="views"
+            onChange={this.changeHandlerInput}
+            value={views}
+          />
+          <button type="submit" className="btn btn-outline-dark">
+            Qo'shish
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AppAddForm;
